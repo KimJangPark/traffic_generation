522b8773fece8936bf5ecb06c5187c512eaf04cf
JungHwan Kim <stevekim01310@gmail.com>
2021-12-15 22:53:01 +0900

    Update request.py

--- request.py
+++ request.py
@@ -4,9 +4,9 @@ import sys
 elapsed_time_total = 0
 img_links = sys.argv[1]
 file = open(img_links, 'r')
-print(img_links)
+NUM_FILES = 40
 
-for i in range (40):
+for i in range (NUM_FILES):
 	l = file.readline()
 	print(l)
 	r = requests.get("http://127.0.0.1:5000/?input_URI=%s"%l)
@@ -18,3 +18,4 @@ for i in range (40):
 	elapsed_time_total += roundtrip
 
 print("total_seconds: ", elapsed_time_total)
+print("average_rtt: ", elapsed_time_total/NUM_FILES)
